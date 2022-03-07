a0160b0d7e763dccf6b25bd1cf6e7b2a0172ea69
bobpruz <bobpruz@gmail.com>
2022-03-07 10:56:23 -0600

    fix numbering add time array

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,5 @@ var weekDayName = moment(myDate).format("dddd");
 // update current day to html
 $("#currentDay").text("Today is: " + weekDayName);
 
+var time = ["06","07","08", "09", "10", "11", "12", "13", "14", "15", "16", "17","18"];
+
